8cd26f17b24e1d20b7e3001c1b066c9145ee4ac5
Ralph_Fonz <raphael@fonseca.tech>
2018-10-23 09:44:18 -0400

    added font-awesome and bootstrap to project

--- low-life-choppper/src/app/app-routing.module.ts
+++ low-life-choppper/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { PodcastComponent } from './podcast/podcast.component';
 
 // Paths
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'podcast', component: PodcastComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   
 ]
